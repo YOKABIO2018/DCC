6aa0b6b5fd7488d683a6eb93dbeb6878bbadddd0
Katafuchi Ryoya <q236018r@mail.kyutech.jp>
2018-09-11 14:48:12 +0900

    loading

--- Web/js/Loading_Show.js
+++ Web/js/Loading_Show.js
@@ -1,15 +1,15 @@
 $(function(){
  $("header").css("display","none");
- //$("main").css("display","none");
- //$("#page_top").css("display","none");
- //$("#main_image").css("display","none");
- //$("footer").css("display","none");
+ $("main").css("display","none");
+ $("#page_top").css("display","none");
+ $("#main_image").css("display","none");
+ $("footer").css("display","none");
 });
 
-$(window).on("load",function(){
-  //$("#main_image").fadeIn("normal");
-  //$("main").fadeIn("slow");
-  //$("#page_top").fadeIn("slow");
-  //$("footer").fadeIn("slow");
+$(window).load(function(){
+  $("#main_image").fadeIn("normal");
+  $("main").fadeIn("slow");
+  $("#page_top").fadeIn("slow");
+  $("footer").fadeIn("slow");
   $("header").delay(600).slideDown("slow");
 });
