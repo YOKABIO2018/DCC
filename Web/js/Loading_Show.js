28067e4d4124d4245e35f235f464276e5f1031cf
Katafuchi Ryoya <q236018r@mail.kyutech.jp>
2018-09-11 16:27:06 +0900

    index%Loading

--- Web/js/Loading_Show.js
+++ Web/js/Loading_Show.js
@@ -1,15 +1,7 @@
 $(function(){
- $("header").css("display","none");
- /*$("main").css("display","none");
- $("#page_top").css("display","none");
- $("#main_image").css("display","none");
- $("footer").css("display","none");*/
-});
-
-$(window).on("load", function(){
-  /*$("#main_image").fadeIn("normal");
-  $("main").fadeIn("slow");
-  $("#page_top").fadeIn("slow");
-  $("footer").fadeIn("slow");*/
-  $("header").delay(600).slideDown("slow");
+  $("#main_image").fadeIn("slow");
+   $("main").fadeIn("slow");
+   $("#page_top").fadeIn("slow");
+   $("footer").fadeIn("slow");
+   $("header").delay(600).slideDown("slow");
 });
